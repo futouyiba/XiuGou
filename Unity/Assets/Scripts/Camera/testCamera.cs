7955153b7d902a8c20f54e6f64b64073d04418f2
Weiland <weilandss@gmail.com>
2022-04-20 21:03:29 +0800

    bolt正常运行了

---
+++ Unity/Assets/Scripts/Camera/testCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ET
+{
+    public class testCamera : MonoBehaviour
+    {
+
+        public int testInt;
+        public GameObject go;
+
+        public void testMet()
+        {
+            go.SetActive(false);
+        }
+    }
+}
