e594c0a0ebdb860bfec671a78e63e58bc105d3ba
Weiland <weilandss@gmail.com>
2022-07-26 11:48:49 +0800

    shader script first pass

---
+++ Unity/Assets/Scripts/Utility/AllIn1SpriteShader/ShaderEffectBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ET.Utility.AllIn1SpriteShader
+{
+    
+    public abstract class ShaderEffectBase : SerializedMonoBehaviour
+    {
+        [Button("ApplyValues")]
+        public abstract void ApplyValues(bool isOn);
+
+        [ReadOnly] public Material mat;
+
+        private void Awake()
+        {
+            mat = GetComponent<Material>();
+        }
+    }
+
+    public class ShaderVar<T>
+    {
+
+        private Action<bool> dlg;
+        private Material mat;
+        [NonSerialized, OdinSerialize]public string name;
+        [NonSerialized, OdinSerialize]public T OnVal;
+        [NonSerialized, OdinSerialize]public T OffVal;
+
+        public ShaderVar(string name, T OnVal, T OffVal, Material mat)
+        {
+            this.name = name;
+            this.OnVal = OnVal;
+            this.OffVal = OffVal;
+            this.mat = mat;
+        }
+
+
+        public void ApplyValue(bool isOn)
+        {
+            T targetVal = isOn ? OnVal : OffVal;
+            if (targetVal is int ival)
+            {
+                mat.SetInt(name, ival);
+            }
+            else if (targetVal is float fval)
+            {
+                mat.SetFloat(name, fval);
+            }
+            else if (targetVal is bool bval)
+            {
+                if(bval) mat.EnableKeyword(name);
+                else mat.DisableKeyword(name);
+                
+            }
+            else if (targetVal is Texture tval)
+            {
+                mat.SetTexture(name,tval);
+            }
+        }
+
+
+    }
+}
---
+++ Unity/Assets/Scripts/Utility/AllIn1SpriteShader/ShaderEffectBlur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sirenix.Serialization;
+
+namespace ET.Utility.AllIn1SpriteShader
+{
+    
+    public class ShaderEffectBlur :ShaderEffectBase
+    {
+        public ShaderVar<float> intensity;
+        public ShaderVar<bool> isLowQuality;
+
+        private void Start()
+        {
+            intensity = new ShaderVar<float>("_Blurintensity", 10, 0, mat);
+            isLowQuality = new ShaderVar<bool>("_BlurHD", true, false, mat);
+        }
+
+        public override void ApplyValues(bool isOn)
+        {
+            intensity.ApplyValue(isOn);
+            isLowQuality.ApplyValue(isOn);
+        }
+    }
+}
